ea71c0f4ab385bb87864b837d330f59ac4f27440
helderalexandrecsoares-max <helderalexandrecsoares@gmail.com>
2026-07-29 01:40:46 +0100

    corrigir caminho dos icones

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'sitrep-v1';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
